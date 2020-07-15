f188bf8230f621eb9cfb9f343dcc6a9988d976fe
prakhil-tp <prakhil.tp@gmail.com>
2020-07-15 22:13:43 +0530

    Add more columns to sheet

--- services/spreadsheets.js
+++ services/spreadsheets.js
@@ -1,18 +1,53 @@
+import moment from "moment";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 import creds from "../client_secret.json";
 
+/**
+ * Closure which serves functions related to sheet actions.
+ * @returns {object} - object of functions
+ */
 export default async function () {
   const doc = new GoogleSpreadsheet(
     "1Oo5udsp58zhR9vjMzyLXtj3vEPl4e_Csn_gaSJSe_EU"
   );
   await doc.useServiceAccountAuth(creds);
   await doc.loadInfo();
-  console.log(`Title: ${doc.title}`);
+  console.log(`Sheet title: ${doc.title}`);
   const sheet = doc.sheetsByIndex[0];
 
-  const addRows = async (data) => {
-    for (let item of data) {
-      await sheet.addRow({ Hashtags: item.name });
+  /**
+   * function to add rows to the sheet.
+   * @function addRows
+   * @param {Array<object>} tweets - Array of tweets
+   * @returns {void}
+   */
+  const addRows = async (tweets) => {
+    const now = moment();
+    const date = now.format("DD/MM/YYYY");
+    const time = now.format("HHmm");
+
+    for (let tweet of tweets) {
+      const {
+        tweetTime,
+        trendHashtag,
+        tweetVolume,
+        tweetText,
+        username,
+        followersCount,
+        retweetCount
+      } = tweet;
+
+      await sheet.addRow({
+        "Date (dd/mm/yyyy)": date,
+        "Time (hhmm - 24hr format)": time,
+        "Tweet Time": tweetTime,
+        "Trend Hashtag": trendHashtag,
+        "Tweet Volume": tweetVolume,
+        "Tweet Text": tweetText,
+        "Tweet Originator": username,
+        "Tweet Originator follower count": followersCount,
+        "Retweet Count": retweetCount
+      });
     }
   };
 
