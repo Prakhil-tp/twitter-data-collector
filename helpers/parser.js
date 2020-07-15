ecbeb0c3ed275d2c1acce84aedc1475161842e31
prakhil-tp <prakhil.tp@gmail.com>
2020-07-15 22:09:48 +0530

    Add function for parse all queries

--- helpers/parser.js
+++ helpers/parser.js
@@ -1,14 +1,26 @@
 /**
  * Helper function which returns array of url query strings
- * @param {object} trending - response of twitter trends API
+ * @param {Array} trends - response of twitter trends API
  * @returns {Array<string>} - returns fist 5 trends' query string as Array.
  */
-const parseTopFiveQueries = (trending) => {
-  if (trending && trending instanceof Array) {
-    const trends = trending[0] && trending[0].trends;
+const parseTopFiveQueries = (trends) => {
+  if (trends instanceof Array && trends.length) {
     return trends.slice(0, 5).map((item) => item.query);
   }
   throw Error("Invalid input");
 };
 
-export default { parseTopFiveQueries };
+/**
+ * Helper function which returns array of url query strings
+ * @function getQueries
+ * @param {Array} trends
+ * @returns {Array<string>} - returns array query collected from the trends
+ */
+const parseQueries = (trends) => {
+  if (trends instanceof Array && trends.length) {
+    return trends.map((trend) => trend.query);
+  }
+  throw Error("Invalid input");
+};
+
+export default { parseTopFiveQueries, parseQueries };
